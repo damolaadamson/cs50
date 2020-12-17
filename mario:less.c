5ac57c164ec2ae8ae82c99cab9d4bbb83026c4c4
damolaadamson <45276988+damolaadamson@users.noreply.github.com>
2020-12-17 16:32:58 -0600

    Create mario:less.c

---
+++ mario:less.c
@@ -0,0 +1,28 @@
+// C program to create a right-aligned pyramid of blocks, similar to those seen in 
+// Nintendo’s Super Mario Brothers Mario, where Mario must ascend the pyramid.
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+    do //do-while loop urges user to input a number between 1-8, inclusive
+    {
+        height = get_int("Height: "); //Asks user for height of pyramid
+    }
+    while (height < 1 || height > 8);
+    
+    for (int i = 1; i <= height; i++) //Loop to make sure the total number of spaces and hashes on each line are not more than 8
+    {
+        for (int j = 0; j < (height - i); j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
